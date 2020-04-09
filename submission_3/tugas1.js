4210db963080fe4d4b0143abf4e6fb35d1f491c0
melonjuice03 <pandupriagungr03@gmail.com>
2020-04-09 13:19:41 +0700

    Tugas 1 Javascript Fundamental

--- submission_3/tugas1.js
+++ submission_3/tugas1.js
@@ -1,1 +1,14 @@
+var readline = require('readline-sync');
+var a = readline.question('Masukkan bilangan bulat: ');
+var b = readline.question('Masukkan bilangan riil: ');
 
+console.log('Sebelum konversi tipe data');
+console.log('Tipe a: '+ typeof a);
+console.log('Tipe b: '+ typeof b);
+
+var konversi_a = Number(a);
+var konversi_b = Number(b);
+
+console.log('Setelah konversi tipe data');
+console.log('Tipe a: '+ typeof konversi_a);
+console.log('Tipe b: '+ typeof konversi_b);
